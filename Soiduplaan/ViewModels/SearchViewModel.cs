543d45ff6160bc7e557d73dbcce167bdb39b10ec
Mihkel Sokk <mihkelsokk@gmail.com>
2011-05-02 19:31:50 +0300

    Added Search View Models

---
+++ Soiduplaan/ViewModels/SearchViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+
+namespace Soiduplaan
+{
+    public class SearchViewModel : INotifyPropertyChanged
+    {
+        public SearchViewModel()
+        {
+            this.AllItems = new ObservableCollection<SearchItemViewModel>();
+            this.StopItems = new ObservableCollection<SearchItemViewModel>();
+            this.RouteItems = new ObservableCollection<SearchItemViewModel>();
+        }
+
+        public ObservableCollection<SearchItemViewModel> AllItems { get; private set; }
+        public ObservableCollection<SearchItemViewModel> StopItems { get; private set; }
+        public ObservableCollection<SearchItemViewModel> RouteItems { get; private set; }
+
+
+        private string _input;
+
+        public string Input
+        {
+            get
+            {
+                return _input;
+            }
+            set
+            {
+                if (value != _input)
+                {
+                    _input = value;
+                    NotifyPropertyChanged("Input");
+                }
+            }
+        }
+
+        //TODO: Here be data loading with JSON.NET
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
