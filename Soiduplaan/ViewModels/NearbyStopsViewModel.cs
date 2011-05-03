4b7e6a690c5c4e7f6c617249e0735b0867a5d1e1
Mihkel Sokk <mihkelsokk@gmail.com>
2011-05-03 16:30:42 +0300

    Nearby ViewModels

---
+++ Soiduplaan/ViewModels/NearbyStopsViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.Device.Location;
+
+namespace Soiduplaan.ViewModels
+{
+    public class NearbyStopsViewModel : INotifyPropertyChanged
+    {
+
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    NotifyPropertyChanged("Title");
+                }
+            }
+        }
+
+        private GeoCoordinate _coordinates;
+
+        public GeoCoordinate Coordinates
+        {
+            get
+            {
+                return _coordinates;
+            }
+            set
+            {
+                if (value != _coordinates)
+                {
+                    _coordinates = value;
+                    NotifyPropertyChanged("Coordinates");
+                }
+            }
+        }
+
+        private int _distance;
+
+        public int Distance
+        {
+            get
+            {
+                return 0; //TODO pass coordinates through getDistance
+            }
+            set
+            {
+                if (value != _distance)
+                {
+                    _distance = value;
+                    NotifyPropertyChanged("Distance");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ Soiduplaan/ViewModels/NearbyViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+
+namespace Soiduplaan.ViewModels
+{
+    public class NearbyViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<NearbyStopsViewModel> NearbyStops { get; private set; }
+
+        //TODO: Load all pins here from JSON
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
