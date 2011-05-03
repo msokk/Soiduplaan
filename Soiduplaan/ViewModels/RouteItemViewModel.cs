7592739c1d8763112950fc5ee55b1c7091bc36be
Martin Lensment <mlensment@gmail.com>
2011-05-03 12:00:11 +0000

    Added 2 new View Models

---
+++ Soiduplaan/ViewModels/RouteItemViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Soiduplaan.ViewModels
+{
+    public class RouteItemViewModel
+    {
+
+        private string _number;
+
+        public string Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                if (value != _number)
+                {
+                    _number = value;
+                    NotifyPropertyChanged("Number");
+                }
+            }
+        }
+
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    NotifyPropertyChanged("Title");
+                }
+            }
+        }
+
+        private string _dueTime;
+
+        public string DueTime
+        {
+            get
+            {
+                return _dueTime;
+            }
+            set
+            {
+                if (value != _dueTime)
+                {
+                    _dueTime = value;
+                    NotifyPropertyChanged("DueTime");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ Soiduplaan/ViewModels/StopViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Device.Location;
+
+
+namespace Soiduplaan.ViewModels
+{
+    public class StopViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<RouteItemViewModel> RouteItems { get; private set; }
+
+        private GeoCoordinate _coordinates;
+
+        public GeoCoordinate Coordinates
+        {
+            get
+            {
+                return _coordinates;
+            }
+            set
+            {
+                if (value != _coordinates)
+                {
+                    _coordinates = value;
+                    NotifyPropertyChanged("Coordinates");
+                }
+            }
+        }
+
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    NotifyPropertyChanged("Title");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
